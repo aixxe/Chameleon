779fde473a7ad4687d487286f539d2e0a6671039
aixxe <aixxe@skyenet.org>
2015-12-18 07:35:45 +0000

    Clean up and update IClientEntity class for new game update.

--- Chameleon/IClientEntity.h
+++ Chameleon/IClientEntity.h
@@ -1,75 +1,83 @@
 #pragma once
 
+#define m_lifeState					0x25B
+#define m_hMyWeapons				0x49F8
+
+#define m_AttributeManager			0x4980
+#define m_Item						0x40
+#define m_iItemDefinitionIndex		0x1D0
+#define m_iItemIDHigh				0x1E8
+#define m_iEntityQuality			0x1D4
+#define m_szCustomName				0x264
+#define m_OriginalOwnerXuidLow		0x4D68
+#define m_OriginalOwnerXuidHigh		0x4D6C
+#define m_nFallbackPaintKit			0x4D70
+#define m_nFallbackSeed				0x4D74
+#define m_flFallbackWear			0x4D78
+#define m_nFallbackStatTrak			0x4D7C
+
 class IClientEntity {
 	public:
 		inline BYTE GetLifeState() {
-			// DT_BasePlayer -> m_lifeState: 0x25B
-			return *(BYTE*)((DWORD)this + 0x25B);
+			// DT_BasePlayer -> m_lifeState
+			return *(BYTE*)((DWORD)this + m_lifeState);
 		}
 
 		inline UINT* GetWeapons() {
-			// DT_BasePlayer -> m_hMyWeapons: 0x2DC8
-			return (UINT*)((DWORD)this + 0x2DC8);
+			// DT_BasePlayer -> m_hMyWeapons
+			return (UINT*)((DWORD)this + m_hMyWeapons);
 		}
 };
 
 class CBaseAttributableItem: IClientEntity {
 	public:
 		inline int* GetItemDefinitionIndex() {
-			// DT_BaseAttributableItem -> m_AttributeManager: 0x2D50
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item: 0x40
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iItemDefinitionIndex: 0x1D0
-			return (int*)((DWORD)this + 0x2D50 + 0x40 + 0x1D0);
+			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iItemDefinitionIndex
+			return (int*)((DWORD)this + m_AttributeManager + m_Item + m_iItemDefinitionIndex);
 		}
 
 		inline int* GetItemIDHigh() {
-			// DT_BaseAttributableItem -> m_AttributeManager: 0x2D50
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item: 0x40
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iItemIDHigh: 0x1E8
-			return (int*)((DWORD)this + 0x2D50 + 0x40 + 0x1E8);
+			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iItemIDHigh
+			return (int*)((DWORD)this + m_AttributeManager + m_Item + m_iItemIDHigh);
 		}
 	
 		inline int* GetEntityQuality() {
-			// DT_BaseAttributableItem -> m_AttributeManager: 0x2D50
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item: 0x40
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iEntityQuality: 0x1D4
-			return (int*)((DWORD)this + 0x2D50 + 0x40 + 0x1D4);
+			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_iEntityQuality
+			return (int*)((DWORD)this + m_AttributeManager + m_Item + m_iEntityQuality);
 		}
 
 		inline char* GetCustomName() {
-			// DT_BaseAttributableItem -> m_AttributeManager: 0x2D50
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item: 0x40
-			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_szCustomName: 0x264
-			return (char*)((DWORD)this + 0x2D50 + 0x40 + 0x264);
+			// DT_BaseAttributableItem -> m_AttributeManager -> m_Item -> m_szCustomName
+			return (char*)((DWORD)this + m_AttributeManager + m_Item + m_szCustomName);
 		}
 
 		inline int* GetOriginalOwnerXuidLow() {
-			// DT_BaseAttributableItem -> m_OriginalOwnerXuidLow: 0x3138
-			return (int*)((DWORD)this + 0x3138);
+			// DT_BaseAttributableItem -> m_OriginalOwnerXuidLow
+			return (int*)((DWORD)this + m_OriginalOwnerXuidLow);
 		}
 
 		inline int* GetOriginalOwnerXuidHigh() {
-			// DT_BaseAttributableItem -> m_OriginalOwnerXuidHigh: 0x313C
-			return (int*)((DWORD)this + 0x313C);
+			// DT_BaseAttributableItem -> m_OriginalOwnerXuidHigh
+			return (int*)((DWORD)this + m_OriginalOwnerXuidHigh);
 		}
 
 		inline int* GetFallbackPaintKit() {
-			// DT_BaseAttributableItem -> m_nFallbackPaintKit: 0x3140
-			return (int*)((DWORD)this + 0x3140);
+			// DT_BaseAttributableItem -> m_nFallbackPaintKit
+			return (int*)((DWORD)this + m_nFallbackPaintKit);
 		}
 
 		inline int* GetFallbackSeed() {
-			// DT_BaseAttributableItem -> m_nFallbackSeed: 0x3144
-			return (int*)((DWORD)this + 0x3144);
+			// DT_BaseAttributableItem -> m_nFallbackSeed
+			return (int*)((DWORD)this + m_nFallbackSeed);
 		}
 
 		inline float* GetFallbackWear() {
-			// DT_BaseAttributableItem -> m_flFallbackWear: 0x3148
-			return (float*)((DWORD)this + 0x3148);
+			// DT_BaseAttributableItem -> m_flFallbackWear
+			return (float*)((DWORD)this + m_flFallbackWear);
 		}
 
 		inline int* GetFallbackStatTrak() {
-			// DT_BaseAttributableItem -> m_nFallbackStatTrak: 0x314C
-			return (int*)((DWORD)this + 0x314C);
+			// DT_BaseAttributableItem -> m_nFallbackStatTrak
+			return (int*)((DWORD)this + m_nFallbackStatTrak);
 		}
 };
