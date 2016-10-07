2744b4b7d7c94556e172846a7343cd6e9831c8b9
aixxe <me@aixxe.net>
2016-10-07 05:21:13 +0100

    Temporary FireEventClientSide hook workaround for new update.

--- Chameleon/FireEventClientSide.h
+++ Chameleon/FireEventClientSide.h
@@ -1,21 +1,23 @@
 #pragma once
 
 // Define the calling convention for the FireEventClientSide function.
-typedef bool(__thiscall *FireEventClientSide)(void*, IGameEvent*);
+typedef bool(__thiscall *FireEventClientSide)(void*, IGameEvent*, char*);
 FireEventClientSide fnOriginalFireEventClientSide = NULL;
 
 // Perform kill icon replacements in here.
-bool __fastcall FireEventClientSideThink(void* ecx, void* edx, IGameEvent* pEvent) {
+bool __fastcall FireEventClientSideThink(void* ecx, void* edx, IGameEvent* pEvent, char* unknown) {
 	// If the event pointer is invalid just call the original.
 	if (!pEvent)
-		return fnOriginalFireEventClientSide(ecx, pEvent); 
+		return fnOriginalFireEventClientSide(ecx, pEvent, unknown); 
 	
 	// Run our replacement function when a "player_death" event is fired.
-	if (!strcmp(pEvent->GetName(), "player_death"))
+	const char* szEventName = pEvent->GetName();
+
+	if (!strcmp(szEventName, "player_death"))
 		ApplyCustomKillIcon(pEvent);
 
 	// Update model indexes when switching servers.
-	if (!strcmp(pEvent->GetName(), "game_newmap")) {
+	if (!strcmp(szEventName, "player_connect")) {
 		// Clear existing values.
 		if (g_ViewModelCfg.size() >= 1)
 			g_ViewModelCfg.clear();
@@ -25,5 +27,5 @@ bool __fastcall FireEventClientSideThink(void* ecx, void* edx, IGameEvent* pEven
 	}
 
 	// Run the original FireEventClientSide function.
-	return fnOriginalFireEventClientSide(ecx, pEvent);
+	return fnOriginalFireEventClientSide(ecx, pEvent, unknown);
 };
