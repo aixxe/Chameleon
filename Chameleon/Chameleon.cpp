7159a2ebe3f7b63ea69db8dd83b22778a30a5ac8
aixxe <me@aixxe.net>
2016-10-29 09:12:21 +0100

    Update IBaseClientDLL interface version.

--- Chameleon/Chameleon.cpp
+++ Chameleon/Chameleon.cpp
@@ -43,7 +43,7 @@ void Initialise() {
 	CreateInterfaceFn fnEngineFactory = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA("engine.dll"), "CreateInterface");
 	
 	// Call "CreateInterface" to get the required class pointers.
-	g_BaseClient = (IBaseClientDLL*)fnClientFactory("VClient017", NULL); 
+	g_BaseClient = (IBaseClientDLL*)fnClientFactory("VClient018", NULL); 
 	g_EntityList = (IClientEntityList*)fnClientFactory("VClientEntityList003", NULL);
 	g_EngineClient = (IVEngineClient*)fnEngineFactory("VEngineClient014", NULL);
 	g_ModelInfo = (IVModelInfoClient*)fnEngineFactory("VModelInfoClient004", NULL);
